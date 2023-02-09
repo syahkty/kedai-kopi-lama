88cf96cae11a69134823f981d2e463fc8c4e781f
syahkty <andisyahktu46@gmail.com>
2023-02-09 20:16:39 +0800

    website kopi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika humberger menu di klik
+document.querySelector("#humberger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menhilangkan humberger menu
+
+const humberger = document.querySelector("#humberger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
